e4c016930d05101e7512ab6a630b9ec7b3e6b236
Alex Halverson <alexhalverson@posteo.net>
2025-11-07 12:36:38 +1100

    feat: add swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,36 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger services in development environment
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc(
+            "v1",
+            new OpenApiInfo
+            {
+                Title = "FaSoLaSearch API",
+                Version = "v1",
+                Description = "API for searching Sacred Harp songs",
+            }
+        );
+    });
+}
+
 var app = builder.Build();
+
+// Configure Swagger middleware in development environment
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FaSoLaSearch API V1");
+    });
+}
 
 app.MapGet("/", () => "Hello World!");
 
